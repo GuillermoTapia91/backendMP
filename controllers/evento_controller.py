c189d106608beb704684d5e8a335379aa434dc65
GuillermoTapia91 <guillermo.tapia1991@gmail.com>
2023-07-10 14:46:00 -0500

    post-get-delete-imagenes-aws en establecimientos y eventos

--- controllers/evento_controller.py
+++ controllers/evento_controller.py
@@ -3,7 +3,12 @@ from config import conexion
 from models.evento_model import EventoModel
 from flask_jwt_extended import get_jwt_identity, jwt_required
 from dtos.evento_dto import EventoRequestDto,EventoResponseDto
+from os import path, getcwd,environ
+from werkzeug.utils import secure_filename
+from uuid import uuid4
+from boto3 import Session
 
+AWSSession = Session(aws_access_key_id=environ.get('AWS_ACCESS_KEY'),aws_secret_access_key=environ.get('AWS_SECRET_KEY'),region_name=environ.get('AWS_BUCKET_REGION'))
 
 class EventosController(Resource):
 
@@ -11,11 +16,15 @@ class EventosController(Resource):
     @jwt_required()
     def get(self):
        
-       print(id)
        resultado = conexion.session.query(EventoModel).all()
-       
+       S3= AWSSession.client('s3') 
        dto = EventoResponseDto(many=True)
        eventos = dto.dump(resultado)
+
+       for evento in eventos:
+        if evento.get('fotoEvento'):  
+          evento['fotoEvento']=S3.generate_presigned_url('get_object',Params={'Bucket':environ.get('AWS_BUCKET_NAME'),'Key':evento.get("fotoEvento")},#ExpiresIn=50
+          )
 
        return {
           'content': eventos
--- dtos/establecimiento_dto.py
+++ dtos/establecimiento_dto.py
@@ -1,14 +1,29 @@
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 from models.establecimiento_model import EstablecimientoModel, TipoEstablecimiento
+from marshmallow import Schema, fields
 from marshmallow_enum import EnumField
 
 #para Crear establecimiento(post)
-class EstablecimientoRequestDto(SQLAlchemyAutoSchema):
-    class Meta:
-        model = EstablecimientoModel
+# class EstablecimientoRequestDto(SQLAlchemyAutoSchema):
+#     class Meta:
+#         model = EstablecimientoModel
 
 #Para listar establecimientos(get)
 class EstablecimientoResponseDto(SQLAlchemyAutoSchema):
     tipoEstablecimiento = EnumField(enum=TipoEstablecimiento,by_value=True)
     class Meta:
         model = EstablecimientoModel
+
+class EstablecimientoRequestDto(Schema):
+    tipoEstablecimiento = EnumField(enum=TipoEstablecimiento,by_value=True)
+    nombre = fields.Str(required=True)
+    distrito = fields.Str(required=True)
+    direccion = fields.Str(required=True)
+    horarioLunes = fields.Str(required=True)
+    horarioMartes = fields.Str(required=True)
+    horarioMiercoles = fields.Str(required=True)
+    horarioJueves = fields.Str(required=True)
+    horarioViernes = fields.Str(required=True)
+    horarioSabado = fields.Str(required=True)
+    horarioDomingo = fields.Str(required=True)
+    paginaWeb = fields.Str(required=True)
